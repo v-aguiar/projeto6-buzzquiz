73a530108d480ad7a0fd3485b9fce21b212cb4b7
v-aguiar <v.aguiar.dev@gmail.com>
2022-02-16 17:58:00 -0300

    Mostrar os quizzes criados caso haja algum. Ao clicar no botão de criar quizz, a primeira tela some e a terceira aparece

--- scripts/script.js
+++ scripts/script.js
@@ -38,32 +38,120 @@ const funcoesDeControle = {
     quizzesContainer.classList.toggle("--escondido")
   },
 
+  toogleTela1SemQuizzes() {
+    const semQuizzes = document.querySelector(".sem-quizzes")
+
+    semQuizzes.classList.toggle("--escondido")
+  },
+
+  toogleTela1SeusQuizzes() {
+    const seusQuizzes = document.querySelector(".seus-quizzes")
+
+    seusQuizzes.classList.toggle("--escondido")
+  },
+
   // Esconder e mostrar a tela 2
   toogleTela2() {
     const respondeQuizz = document.querySelector(".responde-quizz")
 
     respondeQuizz.classList.toggle("--escondido")
+  },
+
+  // Esconder e mostrar a tela 2
+  toogleTela3Parte1() {
+    const criaQuizzParte1 = document.querySelector(".cria-quizz.passo-um")
+
+    criaQuizzParte1.classList.toggle("--escondido")
   }
 }
 
 const funcoesQuizzes = {
   quizzes: [],
+  seus_quizzes: [
+    {
+      title: "meu quizz",
+      image: "https://source.unsplash.com/random"
+    },
+    {
+      title: "meu quizz2",
+      image: "https://source.unsplash.com/random"
+    },
+    {
+      title: "meu quizz3",
+      image: "https://source.unsplash.com/random"
+    }],
+
+  criarQuizz() {
+    funcoesDeControle.toogleTela1();
+    funcoesDeControle.toogleTela3Parte1();
+  },
 
   responderQuizz(quizz) {
-    funcoesDeControle.toogleTela1()
-    funcoesDeControle.toogleTela2()
+    funcoesDeControle.toogleTela1();
+    funcoesDeControle.toogleTela2();
   },
 
   listarTodosOsQuizzes() {
-    console.log('listando todos os Quizzes', this.quizzes)
+    const listaDeTodosOsQuizzes = document.querySelector(".quizzes-todos");
 
-    this.montaEstruturaQuizzPrimeiraTela()
+    listaDeTodosOsQuizzes.innerHTML = funcoesQuizzes.montaEstruturaQuizzPrimeiraTela();
   },
 
   montaEstruturaQuizzPrimeiraTela() {
+    let quizzesEstruturaTela1 = '<li class="quizzes-titulo"><h2>Todos os Quizzes</h2></li>';
 
+    funcoesQuizzes.quizzes.forEach((quizzData) => {
+      const titulo = quizzData.title;
+      const imagem = quizzData.image;
+
+      const quizzEstrutura = `
+        <li class="quizz" onclick="funcoesQuizzes.responderQuizz(this)">
+          <div class="quizz-gradiente"></div>
+          <img src="${imagem}" alt="Imagem de capa do Quizz">
+          <p>${titulo}</p>
+        </li>`;
+
+      quizzesEstruturaTela1 += quizzEstrutura;
+    })
+
+    return quizzesEstruturaTela1;
+  },
+
+  listaSeusQuizzes() {
+    const listaDeSeusQuizzes = document.querySelector(".lista-seus-quizzes");
+
+    if(funcoesQuizzes.seus_quizzes !== null) {
+      funcoesDeControle.toogleTela1SemQuizzes();
+      funcoesDeControle.toogleTela1SeusQuizzes();
+
+      listaDeSeusQuizzes.innerHTML = funcoesQuizzes.montaEstruturaSeusQuizzes();
+    }
+  },
+
+  montaEstruturaSeusQuizzes() {
+    let seusQuizzesEstrutura = `
+  <li class="quizzes-titulo">
+    <h2>Seus Quizzes</h2>
+    <ion-icon name="add-circle" onclick="funcoesQuizzes.criarQuizz()"></ion-icon>
+  </li>`;
+
+    funcoesQuizzes.seus_quizzes.forEach((quizzData) => {
+      const titulo = quizzData.title;
+      const imagem = quizzData.image;
+
+      const quizzEstrutura = `
+        <li class="quizz" onclick="funcoesQuizzes.responderQuizz(this)">
+          <div class="quizz-gradiente"></div>
+          <img src="${imagem}" alt="Imagem de capa do seu Quizz">
+          <p>${titulo}</p>
+        </li>`;
+
+      seusQuizzesEstrutura += quizzEstrutura;
+    })
+
+    return seusQuizzesEstrutura;
   }
 }
 
 funcoesApi.obterQuizzes()
-
+funcoesQuizzes.listaSeusQuizzes();
